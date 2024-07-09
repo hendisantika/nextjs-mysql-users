4b00e95555aab5cdb06377dae65b1e1a51046f9d
hendisantika <hendisantika@yahoo.co.id>
2024-07-09 10:03:34 +0700

    Modify jsconfig.json & next.config.mjs file🫸🌀✏️📗🐧🐳

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+//
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  serverRuntimeConfig: {
+    dbConfig: {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'root', // @@@
+      database: 'users'
+    },
+    secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/api' // development api
+        : 'http://localhost:3000/api' // production api
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
