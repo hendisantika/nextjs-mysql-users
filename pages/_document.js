ae93a77c32b98c3d589f31aa4820913907c90619
hendisantika <hendisantika@yahoo.co.id>
2024-07-09 09:55:33 +0700

    Modify _document.js file🫸🌀✏️📗🐧🐳

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Head, Html, Main, NextScript} from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default Document;
+
+function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                {/* eslint-disable-next-line @next/next/no-css-tags */}
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+            </Head>
+
+            <body>
+            <Main/>
+            <NextScript/>
+
+            {/* credits */}
+            <div className="text-center mt-4">
+                <p>
+                    <a href="https://jasonwatmore.com/next-js-13-mysql-user-registration-and-login-tutorial-with-example-app"
+                       target="_blank">Next.js 13 + MySQL - User Registration and Login Tutorial with Example App</a>
+                </p>
+                <p>
+                    <a href="https://jasonwatmore.com" target="_blank">JasonWatmore.com</a>
+                </p>
+            </div>
+            </body>
+        </Html>
+    );
 }
